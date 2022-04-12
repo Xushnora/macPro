53b3846fbf8a028cf285fb726e2fecbeebcc974e
Xushnora <xushnoranishonova@gmail.com>
2022-04-11 21:03:35 -0700

    changed

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
-const macBox = [
-    {
+const macBox = [{
         id: 1,
         nomi: `Gold`,
         imgUrl: "./imgs/macbookAir.webp",
         imgSlider: ["imgs/macbookAir.webp", "imgs/macPink2.jpg", "imgs/macPink3.jpg", "imgs/macPink4.jpg", "imgs/macPink5.jpg"],
-        ram: [
-            {
+        ram: [{
                 GB: 8,
-                xotira: [
-                    {
+                xotira: [{
                         name: `M1/8/256 Gold`,
                         gbXotira: 256,
                         price: 12497000
@@ -19,11 +16,10 @@ const macBox = [
                         price: 15066000
                     }
                 ]
-            }, 
+            },
             {
                 GB: 16,
-                xotira: [
-                    {
+                xotira: [{
                         name: `M1/16/256 Gold`,
                         gbXotira: 256,
                         price: `16 935 000`
@@ -48,11 +44,9 @@ const macBox = [
         nomi: `Silver`,
         imgUrl: "imgs/macSilver1.jpg",
         imgSlider: ["imgs/macSilver1.jpg", "imgs/macSilver2.jpg", "imgs/macSilver3.jpg", "imgs/macSilver4.jpg", "imgs/macSilver5.jpg"],
-        ram: [
-            {
+        ram: [{
                 GB: 8,
-                xotira: [
-                    {
+                xotira: [{
                         name: `M1/8/256 Silver`,
                         gbXotira: 256,
                         price: 12497000
@@ -68,11 +62,10 @@ const macBox = [
                         price: `22 497 000`
                     }
                 ]
-            }, 
+            },
             {
                 GB: 16,
-                xotira: [
-                    {
+                xotira: [{
                         name: `M1/16/256 Silver`,
                         gbXotira: 256,
                         price: `16 935 000`
@@ -97,11 +90,9 @@ const macBox = [
         nomi: `Grey`,
         imgUrl: "imgs/macGrey1.jpg",
         imgSlider: ["imgs/macGrey1.jpg", "imgs/macGrey2.jpg", "imgs/macGrey3.jpg", "imgs/macGrey4.jpg", "imgs/macGrey5.jpg"],
-        ram: [
-            {
+        ram: [{
                 GB: 8,
-                xotira: [
-                    {
+                xotira: [{
                         name: `M1/8/256 Grey`,
                         gbXotira: 256,
                         price: 12497000
@@ -112,11 +103,10 @@ const macBox = [
                         price: 15066000
                     }
                 ]
-            }, 
+            },
             {
                 GB: 16,
-                xotira: [
-                    {
+                xotira: [{
                         name: `M1/16/256 Grey`,
                         gbXotira: 256,
                         price: `16 935 000`
@@ -138,11 +128,123 @@ const macBox = [
     }
 ];
 
+const rightBox = document.querySelector('.rightBox');
+
+// RENDER BTN block
+
+function renderButtons() {
+    let box = document.createElement('div');
+    box.innerHTML = `
+        <div>
+        <h1 class="mac__title">MacBook Air 13-inch</h1>
+        <div class="macNameBox">
+            <h2 class="macName">M1/8/512 Gold</h2>
+        </div>
+        <div class="ram">
+            <h4 class="ram__title">Ram</h4>
+            <div class="ramBtnBox">
+                <button id="ramOne" class="ramBtn borderActive">8GB</button>
+                <button id="ramTwo" class="ramBtn">16GB</button>
+            </div>
+        </div>
+        <div class="xotira">
+            <h4 class="ram__title">Xotira hajmi</h4>
+            <div class="ramBtnBox">
+                <button id="memoryOne" class="ramBtn borderActive">256GB</button>
+                <button id="memoryTwo" class="ramBtn">512GB</button>
+                <button id="memoryThree" class="ramBtn noneBtn">1TB</button>
+            </div>
+        </div>
+        <div class="color">
+            <h4 class="color__title">Ranglar</h4>
+            <div class="colorBtnBox">
+                <button class="goldBtn color-btn borderActive"> 
+                    <span class="yellow"></span>
+                    <p class="color-name">Tilla rang</p>
+                </button>
+                <button class="grayBtn color-btn">
+                    <span class="gray"></span>
+                    <p class="color-name">Kumush rang</p>
+                </button>
+                <button class="lightGrayBtn color-btn">
+                    <span class="lightGray"></span>
+                    <p class="color-name"> Kosmik kulrang</p>
+                </button>
+            </div>
+        </div>
+        <div class="addBox">
+            <div class="add-btn-box">
+                <button class="minusBtn">
+                    <i class='bx bx-minus'></i>
+                </button>
+                <input class="numberInput" type="number" value="1">
+                <button class="plusBtn">
+                    <i class='bx bx-plus'></i>
+                </button>
+            </div>
+        </div>
+        <div class="price">
+            <h3 class="price-title">15 066 000 so'm</h3>
+            <p class="price-text">17 627 500 so'm</p>
+        </div>
+        <button class="add-basket-btn">Savatchaga qo'shish</button>
+        <button class="combars-btn">Taqqoslash</button>
+    </div>
+        `
+    rightBox.appendChild(box);
+}
+
+renderButtons();
+
+// RENDER IMG block
+
+const leftBox = document.querySelector('.leftBox');
+
+function renderImg() {
+    let box = document.createElement('div');
+    box.innerHTML = `
+    <div>
+        <div class="macBox">
+            <img class="macImg" src="./imgs/macbookAir.webp" alt="mac">
+        </div>
+        <div class="desc-modalBox">
+            <button class="decs-modal">
+                <i class='bx bx-fullscreen'></i>
+            </button>
+        </div>
+        <div class="slider-img">
+            <ul class="slider-list">
+                <li class="slider-item active-item">
+                    <img src="imgs/macbookAir.webp" alt="mac">
+                </li>
+                <li class="slider-item">
+                    <img src="imgs/macPink2.jpg" alt="mac">
+                </li>
+                <li class="slider-item">
+                    <img src="imgs/macPink3.jpg" alt="mac">
+                </li>
+                <li class="slider-item">
+                    <img src="imgs/macPink4.jpg" alt="mac">
+                </li>
+                <li class="slider-item">
+                    <img src="imgs/macPink5.jpg" alt="mac">
+                </li>
+            </ul>
+        </div>
+    </div>
+    `
+    leftBox.appendChild(box);
+}
+
+renderImg()
+
+
 // >>>>>>>>>>>>>>>> RAM BTN <<<<<<<<<<<<<<<<<<<
 
 const ramOneBtn = document.querySelector('#ramOne');
 const ramTwoBtn = document.querySelector('#ramTwo');
 const macTitle = document.querySelector('.macName');
+const priceTitle = document.querySelector('.price-title');
 
 ramOneBtn.addEventListener('click', renderOne);
 ramTwoBtn.addEventListener('click', renderTwo);
@@ -214,6 +316,7 @@ function goldColor() {
     macsBox.innerHTML = `<img class="macImg" src="${macBox[0].imgUrl}" alt="mac">`
 
 }
+
 function silverColor() {
     silverBtn.style.borderColor = "blue";
     greyBtn.style.borderColor = "#888";
@@ -222,6 +325,7 @@ function silverColor() {
     macsBox.innerHTML = `<img class="macImg" src="${macBox[1].imgUrl}" alt="mac">`
 
 }
+
 function greyColor() {
     goldBtn.style.borderColor = "#888";
     silverBtn.style.borderColor = "#888";
@@ -230,3 +334,12 @@ function greyColor() {
     macsBox.innerHTML = `<img class="macImg" src="${macBox[2].imgUrl}" alt="mac">`
 
 }
+
+
+// >>>>>>>>>>>> PRICE HISOBLASH <<<<<<<<<<<<<<<<
+
+const minusBtn = document.querySelector('.minusBtn');
+const plusBtn = document.querySelector('.plusBtn');
+const numberInput = document.querySelector('.numberInput');
+
+console.log(numberInput.value);
